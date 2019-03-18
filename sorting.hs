f1dd4151c861b756816883beeba3eae9718ad31b
chairmanagha <34392191+chairmanagha@users.noreply.github.com>
2019-03-19 01:00:27 +0500

    Update sorting.hs

--- sorting.hs
+++ sorting.hs
@@ -1,3 +1,6 @@
+--practice of all the common sorting algorithms
+
+
 insert_sorted :: [Int] -> Int -> [Int]
 insert_sorted = \list -> \v -> 
  case list of
@@ -16,3 +19,25 @@ selection_sort = \list ->
  case list of
   [] -> []
   _ -> find_min list : selection_sort(remove_one list (find_min list))
+
+merge :: [Int] -> [Int] -> [Int]
+merge [] ys = ys
+merge xs [] = xs
+merge (x:xs) (y:ys) 
+ | x <= y = x:merge xs (y:ys)
+ | otherwise = y:merge (x:xs) ys
+
+merge_sort :: [Int] -> [Int]
+merge_sort [] = []
+merge_sort [x] = [x]
+merge_sort l = merge (merge_sort (front l)) (merge_sort (back l))
+ where 
+  front l = take ((length l) `div` 2) l
+  back l = drop ((length l) `div` 2) l
+
+quick_sort :: [Int] -> [Int]
+quick_sort [] = []
+quick_sort (x:xs) = (quick_sort lower) ++ [x] ++ (quick_sort upper) 
+ where
+  lower = [y | y <- xs, y <= x]
+  upper = [y | y <- xs, y > x]
